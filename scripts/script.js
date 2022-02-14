cd34fed9ff6b539e76db957ab8ca1b7c68f559d3
Irshman <76690755+Irshman@users.noreply.github.com>
2022-02-14 16:28:52 +0200

    fist commit

---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+// c05f4ef647660e16f03781574660274c
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+let input = document.querySelector("#enterCity");
+let btn = document.querySelector(".weather-page__btn");
+let nameCity = document.querySelector(".weather-page__title");
+let descriptionWether = document.querySelector(".weather-page__description");
+let cityTemp = document.querySelector(".weather-page__deg");
+let cityWindSpeed = document.querySelector(".weather-page__wind-speed");
+let pressure = document.querySelector(".weather-page__pressure");
+
+
+
+let city = 'London';
+
+btn.addEventListener('click', () => {
+    if (input.value === "") {
+        alert("This city is not found!")
+    } else {
+        city = input.value;
+    }
+})
+
+function init() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c05f4ef647660e16f03781574660274c`)
+    .then(resp => { return resp.json()})
+    .then(data => {
+        nameCity.innerHTML = data.name;
+        descriptionWether.innerHTML = data.weather[0].description;
+        cityTemp.innerHTML = `${temperature()}°`;
+        cityWindSpeed.innerHTML = `Wind speed: ${data.wind.speed} m/s`;
+        pressure.innerHTML = `Pressure: ${data.main.pressure} hPa`;
+
+        console.log(data);
+
+        let getWether = data.weather[0].main;
+
+        if(getWether === 'Rain') {
+            document.body.style.background = 
+        'url(../img/rain.jpg)';
+        } else if(getWether === 'Clear') {
+            document.body.style.background = 
+        'url(../img/sunny.jpg) center / cover no-repeat';
+        } else if(getWether === 'Extreme') {
+            document.body.style.background = 
+        'url(../img/thand.jpg) center / cover no-repeat';
+        } else if(getWether === 'Snow') {
+            document.body.style.background = 
+        'url(../img/snowy.jpg) center / cover no-repeat';
+        } else {
+            document.body.style.background = 
+        'url(../img/always.jpg) center / cover no-repeat';
+        }
+
+    
+
+        function temperature() {
+            let getTemp = data.main.temp;
+            let tempC = Math.floor(getTemp) - 273;
+            return tempC
+        }
+    })
+}
+
+init();
+
+setInterval(() => {
+    init()
+}, 10000);
