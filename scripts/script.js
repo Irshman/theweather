3605d32b477a6c942db60e03085a9b3b657c28db
Irshman <76690755+Irshman@users.noreply.github.com>
2022-02-15 12:45:35 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -33,19 +33,19 @@ async function init() {
 
         if(getWether === 'Rain') {
             document.body.style.background = 
-        'url(ttps://github.com/Irshman/theweather/blob/main/img/rain.jpg) center / cover no-repeat';
+        'url(./img/rain.jpg) center / cover no-repeat';
         } else if(getWether === 'Clear') {
             document.body.style.background = 
-        'url(ttps://github.com/Irshman/theweather/blob/main/img/sunny.jpg) center / cover no-repeat';
+        'url(./img/sunny.jpg) center / cover no-repeat';
         } else if(getWether === 'Extreme') {
             document.body.style.background = 
-        'url(ttps://github.com/Irshman/theweather/blob/main/img/thand.jpg) center / cover no-repeat';
+        'url(./img/thand.jpg) center / cover no-repeat';
         } else if(getWether === 'Snow') {
             document.body.style.background = 
-        'url(ttps://github.com/Irshman/theweather/blob/main/img/snowy.jpg) center / cover no-repeat';
+        'url(./img/snowy.jpg) center / cover no-repeat';
         } else {
             document.body.style.background = 
-        'url(https://github.com/Irshman/theweather/blob/main/img/always.jpg) center / cover no-repeat';
+        'url(./img/always.jpg) center / cover no-repeat';
         }
 
         function temperature() {
